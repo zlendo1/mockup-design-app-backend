150106a01d4fb53a716fccfdf5e86889c0bb1ce8
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-10 23:54:41 +0200

    added userController with getUser

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const db = require('../config/db.js');
+
+const { generateServerErrorResponse } = require('../utils/messages.js');
+
+async function getUser(req, res) {
+	const userId = req.params.userId;
+
+	try {
+		const foundUser = await db.user.findOne({ where: { id: userId } });
+
+		if (!foundUser) {
+			res.status(400).json({ message: 'User not found' });
+
+			return;
+		}
+
+		res.status(200).json({ userId: foundUser.id, username: foundUser.username });
+	} catch (error) {
+		res.status(500).json(generateServerErrorResponse(error));
+	}
+}
+
+module.exports = {
+	getUser
+};
---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userController = require('../controllers/userController.js');
+
+const authMiddleware = require('../middleware/authMiddleware.js');
+
+router.use(authMiddleware.addJwtHeader);
+
+router.get('/user', userController.getUser);
+
+module.exports = router;
