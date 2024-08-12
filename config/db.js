808772c4ad4a35d7b0d06cfc8809e06901b389bd
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-12 15:52:04 +0200

    changed mysql for postgres

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize')
-const mysql2 = require('mysql2');
+import { Sequelize } from 'sequelize';
+import { PostgresDialect } from '@sequelize/postgres'
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
-	{ host: process.env.DB_HOST, dialect: "mysql", dialectModule: mysql2, logging: false }
+	{ host: process.env.DB_HOST, dialect: "postgres", dialectModule: PostgresDialect, logging: false }
 );
 
 const db = {};
