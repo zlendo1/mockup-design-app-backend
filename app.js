ea10dc4cf1bcea5cf831214a5f5065017d4c6280
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-12 18:28:37 +0200

    added correct app components

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const session = require('express-session');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const db = require('./config/db.js');
 
-var app = express();
+const authMiddleware = require('./middleware/authMiddleware');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-app.use(logger('dev'));
+const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: process.env.SESSION_SECRET
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+app.use(cors());
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/auth', authRoutes);
+app.use('/user', authMiddleware.verifyJWT, userRoutes);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+await db.sequelize.sync();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
