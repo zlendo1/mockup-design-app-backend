fb1c4601176afd57312643e9ef1ad9aaa408e61c
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-13 22:57:21 +0200

    removed dialectModule explicit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
-const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,
 	{
 		host: process.env.POSTGRES_HOST,
 		dialect: "postgres",
-		dialectModule: pg,
 		logging: false
 	}
 );
