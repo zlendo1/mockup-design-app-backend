b5a31b6313a064b4b2125c220bd00ad52335dee2
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-13 18:07:06 +0200

    added /register route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const authController = require('../controllers/authController.js');
 
 router.post('/login', authController.login);
+router.post('/register', authController.register);
 
 module.exports = router;
