4f4d0e0e5385c0e3adf61a63799da9ef54635852
zlendo1 <zlendo1@etf.unsa.ba>
2024-09-10 14:31:30 +0200

    changed unnecessary association

--- config/db.js
+++ config/db.js
@@ -15,7 +15,6 @@ db.sequelize = sequelize
 db.user = require('../models/User.js')(sequelize, Sequelize)
 db.project = require('../models/Project.js')(sequelize, Sequelize)
 
-db.project.hasOne(db.user)
 db.user.hasMany(db.project)
 
 module.exports = db
